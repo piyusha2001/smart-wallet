ab394d9925a38ec0fa414584ac43c9a1f5220d57
piyusha2001 <ppiyusha2001@gmail.com>
2022-02-06 03:02:29 +0530

    title prop passed

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,11 +2,11 @@ import { Card, CardContent, CardHeader, Typography } from '@material-ui/core';
 import React from 'react';
 import useStyles from './styles';
 
-function Details() {
+function Details({ title }) {
 	const classes = useStyles();
 	return (
 		<Card className={classes.income}>
-			<CardHeader title='Income' />
+			<CardHeader title={title} />
 			<CardContent>
 				<Typography variant='h5'>$50</Typography>
 				{/* <Doughnut data='Data' /> */}
