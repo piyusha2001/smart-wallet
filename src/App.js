ae790f436babd0bc928a4eaba0ed649449ea9da4
piyusha2001 <ppiyusha2001@gmail.com>
2022-02-06 02:47:22 +0530

    Basic Layout created using grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
-
+import Details from './components/Details/Details';
 function App() {
-	return <div>hello</div>;
+	return (
+		<div>
+			<Grid
+				container
+				spacing={0}
+				alignItems='center'
+				justifyContent='center'
+				style={{ height: '100vh' }}
+			>
+				<Grid items xs={12} sm={4}>
+					<Details />
+				</Grid>
+				<Grid items xs={12} sm={4}>
+					Main
+				</Grid>
+				<Grid items xs={12} sm={4}>
+					<Details />
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
 
 export default App;
