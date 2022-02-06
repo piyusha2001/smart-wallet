abb96e2b328ba7606e104d9a9db2e184d2e2ac15
piyusha2001 <ppiyusha2001@gmail.com>
2022-02-07 01:32:38 +0530

    chartdata and total added

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,18 +1,21 @@
 import { Card, CardContent, CardHeader, Typography } from '@material-ui/core';
 import React from 'react';
 import useStyles from './styles';
-
-function Details({ title }) {
+import { Doughnut } from 'react-chartjs-2';
+import useTransactions from '../../useTransactions';
+const DetailsCard = ({ title, subheader }) => {
+	const { total, chartData } = useTransactions(title);
 	const classes = useStyles();
+
 	return (
 		<Card className={title === 'Income' ? classes.income : classes.expense}>
-			<CardHeader title={title} />
+			<CardHeader title={title} subheader={subheader} />
 			<CardContent>
-				<Typography variant='h5'>$50</Typography>
-				{/* <Doughnut data='Data' /> */}
+				<Typography variant='h5'>${total}</Typography>
+				<Doughnut data={chartData} />
 			</CardContent>
 		</Card>
 	);
-}
+};
 
-export default Details;
+export default DetailsCard;
