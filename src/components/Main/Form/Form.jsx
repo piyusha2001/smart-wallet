851eb753ba919ad25cc3ec69049219b475277cfe
piyusha2001 <ppiyusha2001@gmail.com>
2022-02-24 01:41:14 +0530

    voice detection to fill form logic added

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -9,7 +9,7 @@ import {
 	Typography,
 } from '@material-ui/core';
 import { useSpeechContext } from '@speechly/react-client';
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import {
 	expenseCategories,
@@ -40,6 +40,76 @@ function Form() {
 		addTransaction(transaction);
 		setFormData(initialState);
 	};
+
+	useEffect(() => {
+		if (segment) {
+			if (segment.intent.intent === 'add_expense') {
+				setFormData({ ...formData, type: 'Expense' });
+			} else if (segment.intent.intent === 'add_income') {
+				setFormData({ ...formData, type: 'Income' });
+			} else if (
+				segment.isFinal &&
+				segment.intent.intent === 'create_transaction'
+			) {
+				return createTransaction();
+			} else if (
+				segment.isFinal &&
+				segment.intent.intent === 'cancel_transaction'
+			) {
+				return setFormData(initialState);
+			}
+
+			segment.entities.forEach((s) => {
+				const category = `${s.value.charAt(0)}${s.value
+					.slice(1)
+					.toLowerCase()}`;
+
+				switch (s.type) {
+					case 'amount':
+						setFormData({ ...formData, amount: s.value });
+						break;
+					case 'category':
+						if (
+							incomeCategories
+								.map((iC) => iC.type)
+								.includes(category)
+						) {
+							setFormData({
+								...formData,
+								type: 'Income',
+								category,
+							});
+						} else if (
+							expenseCategories
+								.map((iC) => iC.type)
+								.includes(category)
+						) {
+							setFormData({
+								...formData,
+								type: 'Expense',
+								category,
+							});
+						}
+						break;
+					case 'date':
+						setFormData({ ...formData, date: s.value });
+						break;
+					default:
+						break;
+				}
+			});
+
+			if (
+				segment.isFinal &&
+				formData.amount &&
+				formData.category &&
+				formData.type &&
+				formData.date
+			) {
+				createTransaction();
+			}
+		}
+	}, [segment]);
 
 	const selectedCategories =
 		formData.type === 'Income' ? incomeCategories : expenseCategories;
