29c37aeba669c4721c8bd73fb40b70f20458148c
piyusha2001 <ppiyusha2001@gmail.com>
2022-02-24 02:37:49 +0530

    scroll logic added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { Grid } from '@material-ui/core';
+import { SpeechState, useSpeechContext } from '@speechly/react-client';
 import {
 	ErrorPanel,
 	PushToTalkButton,
 	PushToTalkButtonContainer,
 } from '@speechly/react-ui';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Details from './components/Details/Details';
 import Main from './components/Main/Main';
 import useStyles from './styles';
 function App() {
 	const classes = useStyles();
+	const { speechState } = useSpeechContext();
+	const main = useRef(null);
+
+	const executeScroll = () => main.current.scrollIntoView();
+
+	useEffect(() => {
+		if (speechState === SpeechState.Recording) {
+			executeScroll();
+		}
+	}, [speechState]);
 	return (
 		<div>
 			<Grid
@@ -20,7 +31,7 @@ function App() {
 				justifyContent='center'
 				style={{ height: '100vh' }}
 			>
-				<Grid items xs={12} sm={4} className={classes.mobile}>
+				<Grid ref={main} item xs={12} sm={4} className={classes.mobile}>
 					<Details title='Income' />
 				</Grid>
 				<Grid items xs={12} sm={3} className={classes.main}>
